2acd9853e3a5efd3eb8c5aa2204b8b9eda6b2091
azlan <mhdazlanabaziz@gmai.com>
2020-06-04 12:14:48 +0800

    adding static folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const debug = require('debug')('PMU_debugger');
+const path = require('path');
 
 const app = express();
 
@@ -9,12 +10,18 @@ app.use(morgan('dev'));
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'./static')));
 app.get('/', (req, res) => {
   // use ctrl+cmd+space to insert emoji
-  res.send('Hello Express 😃');
+  // res.send('Hello Express 😃');
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
-  global.console.log(`Express server listnening to port ${chalk.rgb(123,45,67).underline(port)}`);
-// user curl to test in terminal window
+  global.console.log(`Express server listnening to port ${chalk.rgb(123, 45, 67).underline(port)}`);
+// user curl to test in terminal window eg: curl localhost:3000
 });
